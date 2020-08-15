d18f4b9a50a0bea1ce2aa8bb4b61a7edf5951925
Danilo Carlos Marques <danilo.marques@presidencia.gov.br>
2020-08-14 21:10:53 -0300

    Mini aplicacao de player de video em React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import VideoList from "./ui/VideoList";
+import NewForm from "./ui/NewForm";
+import VideoPlayer from "./ui/VideoPlayer";
+import VideoContext from "./data/video/VideoContext";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <VideoContext>
+                <VideoPlayer />
+                <NewForm />
+                <VideoList />
+            </VideoContext>
+        </div>
+    );
 }
 
 export default App;
